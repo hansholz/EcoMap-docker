3a2f6219fc96fe636649d44323d1198f270148bf
hansholz <48967084+hansholz@users.noreply.github.com>
2020-09-09 02:23:02 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-if [[ -z $ECOMAP_DB_HOST || -z $ECOMAP_DB_PASSWORD ]]; then
-	>&2 echo "Environment variables ECOMAP_DB_HOST and ECOMAP_DB_PASSWORD are required"
-	exit 1
-else
-	echo -e "rw.host=$ECOMAP_DB_HOST\n\
-rw.password=$ECOMAP_DB_PASSWORD\n\
-ro.host=$ECOMAP_DB_HOST\n\
-ro.password=$ECOMAP_DB_PASSWORD" >> /opt/EcoMap/ecomap/etc/db.conf
-fi
-
 httpd -D FOREGROUND -e info
